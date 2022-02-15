369ee051f656f38822390b2b61ae1538c85f70f7
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-15 21:24:05 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,24 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map((item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let instance = new SimpleLightbox('.gallery a');
+  instance.open(); 
