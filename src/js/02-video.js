0f94920db25ddd228ebad8ace905753f785048bf
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-16 03:31:22 +0200

    throttle is added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,34 @@
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(iframe);
 
 player.on('play', function() {
         console.log('played the video!');
     });
 
-player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.on('timeupdate', _.throttle(function(data) {
+    // data is an object containing properties specific to that event
+    let time = 0;
+    time += data.seconds;
+    console.log(time);
+    localStorage.setItem("videoplayer-current-time", time);
+}, 1000));
 
-console.log(player.getPaused());
+// player.getCurrentTime().then(function(seconds) {
+//     // seconds = the current playback position
+// }).catch(function(error) {
+//     // an error occurred
+// });
+
+// var throttle = require('lodash.throttle');
+
+// const thr = _.throttle(() => {
+//     console.log("Scroll handler call every 300ms");
+// }, 300);
+// console.log(thr);
+
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     console.log("Scroll handler call every 300ms");
+//   }, 300)
+// );
